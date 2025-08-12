e1b552d632e68bea16b5f9fcc11862f0c4aad253
LyrinePoliarco <152787755+LyrinePoliarco@users.noreply.github.com>
2025-08-12 16:10:38 +0800

    Create Activity1.php

---
+++ Activity1.php
@@ -0,0 +1,207 @@
+<?php
+?>
+
+<html>
+    <head>
+      <title>BOOTSTRAP</title>
+      <link rel= "stylesheet" href="css/bootstrap.min.css">
+      <script src="js/query.js"></script>
+      <script src="js/bootstrap.js"></script>
+
+
+</head>
+    <body>
+     <h1>Activity 1</h1>
+     
+     <div class = "container-fluid">
+     <table class = "table">
+      <tr class = "danger">
+        <th>ID</th>
+        <th>Name</th>
+        <th>Remarks</th>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>Jhoanna</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>002</td>
+        <td>Aya</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>003</td>
+        <td>Luna</td>
+        <td class = "danger">Failed</td>
+      </tr>
+     
+    
+    </table>
+      <table class = "table table-striped">
+      <tr class = "info">
+        <th>ID</th>
+        <th>Name</th>
+        <th>Remarks</th>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>Jhoanna</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>002</td>
+        <td>Aya</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>003</td>
+        <td>Luna</td>
+        <td>Failed</td>
+      </tr>
+     
+    
+    </table>
+    <table class = "table table-condensed">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Remarks</th>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>Jhoanna</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>002</td>
+        <td>Aya</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>003</td>
+        <td>Luna</td>
+        <td>Failed</td>
+      </tr>
+     
+    
+    </table>
+    <table class = "table table-hover table-bordered">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Remarks</th>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>Jhoanna</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>002</td>
+        <td>Aya</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>003</td>
+        <td>Luna</td>
+        <td>Failed</td>
+      </tr>
+     
+    
+    </table>
+    <table class = "table table-bordered">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Remarks</th>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>Jhoanna</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>002</td>
+        <td>Aya</td>
+        <td>Passed</td>
+      </tr>
+      <tr>
+        <td>003</td>
+        <td>Luna</td>
+        <td>Failed</td>
+      </tr>
+     
+    
+    </table>
+     </div>
+     
+     <hr/>
+     <br/>
+     <div class = "container">
+      <p class ="text-success bg-success">Success</p>
+      <p class ="text-danger bg-danger">Danger</p>
+      <p class ="text-info bg-info">Info</p>
+      <p class ="text-primary bg-primary">Primary</p>
+      <p class ="text-muted">Muted</p>
+      <p class ="text-warning bg-warning">Warning</p>
+
+      <p class ="lead">Computer Science</p>
+      <p class ="small">Computer Science</p>
+      <p class ="text-center">Computer Science</p>
+      <p class ="text-right">Computer Science</p>
+      <p class ="text-uppercase">Computer Science</p>
+      <p class ="text-lowercase">Computer Science</p>
+     </div>
+
+     
+     
+     
+     <hr/>
+     <br/>
+     <div class = "container-fluid">
+      <div class = "row">
+      <div class = "col-md-1" style="background: green;">grid</div>
+      <div class = "col-md-1" style="background: red;">grid</div>
+      <div class = "col-md-5" style="background: blue;">grid</div>
+        </div>
+        <div class ="row">
+          <div class = "col-md-6" style="background: orange;">grid</div>
+
+        </div>
+
+     </div>
+     
+     
+     <hr/>
+     <br/>
+     <div class ="container-fluid">
+      <header class= "jumbotron">
+        <h1>Container Fluid</h1>
+     </div>
+
+
+     <hr/>
+     <br/>
+     <div class ="container">
+      <header class= "jumbotron">
+        <h1>Container</h1>
+     </div>
+
+
+     <hr/>
+     <br/>
+
+      <div class='jumbotron'>
+          <h1>Hello</h1>
+     </div>
+</body>
+
+  </html>
+
+
+
+
+apps-fileview.texmex_20250206.00_p0
+Activity1.php
+Displaying Activity1.php.
